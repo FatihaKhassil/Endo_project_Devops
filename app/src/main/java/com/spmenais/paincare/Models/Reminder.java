0c0c4aea4ac9ed47af73a9bf6a361d4f329e61f5
FatihaKhassil <fatiha321kh@gmail.com>
2026-03-21 22:44:32 +0100

    test: introduce Java error for build 4

--- app/src/main/java/com/spmenais/paincare/Models/Reminder.java
+++ app/src/main/java/com/spmenais/paincare/Models/Reminder.java
@@ -3,6 +3,7 @@ package com.spmenais.paincare.Models;
 public class Reminder {
     private String id;
     private String title;
+    ERREUR_INTENTIONNELLE_POUR_TEST
     private String time;
     private boolean isActive;
     private boolean[] repeatDays;
